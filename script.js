2d1df5caf3c284a7392be2655e9b47bd2c44e17d
Oskaras <Oskaras741@gmail.com>
2026-01-22 21:26:38 +0200

    Seni...

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const carForm = document.getElementById("carForm");
 const carsDiv = document.getElementById("cars");
+const istrintiprice = document.getElementById("istrinti-price");
 carForm.addEventListener("submit", function (event) {
   // Sustabdys standartine formos elgseną, neperkraus puslapio
   event.preventDefault();
-  const name = document.getElementById("name").value;
+  const name = document.getElementById("name").value.trim();
   const description = document.getElementById("description").value;
   const price = document.getElementById("price").value;
   const image = document.getElementById("image").value;
@@ -20,7 +21,24 @@ carForm.addEventListener("submit", function (event) {
   <p>Automobilio kaina: ${price}</p>
   <p>Automobilio kuras: ${fuel}</p>`;
 
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Ištrinti";
+  deleteBtn.className = "delete-btn";
+
+  deleteBtn.addEventListener("click", function () {
+    carCard.remove();
+  });
+  if (name.length < 3) {
+    alert("Per trumpas automobilio pavadinimas");
+    return;
+  }
+  if (price <= 0) {
+    alert("Per maža kaina");
+    return;
+  }
+  carCard.appendChild(deleteBtn);
   carsDiv.appendChild(carCard);
+
   // Atstatyk formą į pradinę padėtį
   carForm.reset;
 });
